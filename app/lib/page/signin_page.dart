ec0980224c48ac1beeb976962837062f0fe8b0f7
Leonardo Chen <pingta007@gmail.com>
2023-07-07 20:22:25 +0800

    changes

--- app/lib/page/signin_page.dart
+++ app/lib/page/signin_page.dart
@@ -2,6 +2,17 @@ import "package:flutter/material.dart";
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
+  var _unameController = new TextEditingController();
+  var _passwdController = new TextEditingController();
+
+  void initState() {
+    _unameController.addListener(() {
+      print(_unameController.text);
+    });
+    _passwdController.addListener(() {
+      print(_passwdController.text);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,14 +20,25 @@ class SignInPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Sign In"),
       ),
-      body: Center(
+      body: const Center(
           child: Column(
             children: <Widget>[
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(context, "signin返回值");
-                },
-                icon: const Icon(Icons.arrow_back),
+              TextField(
+                autofocus: true,
+                controller: _unameController,
+                decoration: InputDecoration(
+                    labelText: "用户名",
+                    hintText: "用户名或邮箱",
+                    prefixIcon: Icon(Icons.person)
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    labelText: "密码",
+                    hintText: "您的登录密码",
+                    prefixIcon: Icon(Icons.lock)
+                ),
+                obscureText: true,
               ),
             ],
           )),
